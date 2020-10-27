943e37b87e4416aaceb61455505de12a25d93fa0
S-bite <you@example.com>
2020-10-27 23:34:46 +0900

    change code.c

--- code.c
+++ code.c
@@ -5,6 +5,10 @@ int main(int argc, char* argv[])
 {
     int x = atoi(argv[1]);
     int y = atoi(argv[2]);
+    if (y == 0) {
+        printf("can't divide by zero!\n");
+        return 0;
+    }
     printf("%d / %d = %d\n", x, y, x / y);
     return 0;
 }
